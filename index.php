ed96c284ba1197f6fe7ec8cc6aac752e21aa6a4f
Ram1607 <sairamsudireddy7@gmail.com>
2019-01-27 17:51:33 +0530

    Registration form created

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="UTF-8">
-  <title>Login Form</title>
+  <title>Student Portal</title>
   
   
   
@@ -13,57 +13,123 @@
 </head>
 
 <body style="background-color:#1c2b4b">
+
+
+
 	<div class="nav">
 		<div class="webtitle-screen">
 			<div class="webtitle">
-				<h1>National Institute of Technology, Durgapur</h1>
+				<h2>National Institute of Technology, Durgapur</h2>
 			</div>
 		</div>
 		<div class="nav-menu">
-		<ul>
-			<li><a href="#">Home</a></li>
-			<li><a href="#">About Us</a></li>
-			<li><a href="#">Notice</a></li>
-			<li><a href="#">Download</a></li>
-			<li id="myBtn"><a>Login</a></li>
-		</ul>
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About Us</a></li>
+				<li><a href="#">Notice</a></li>
+				<li><a href="#">Download</a></li>
+				<li id="Btn"><a>Login</a></li>
+				<li id="signupBtn"><a>Sign Up</a></li>
+			</ul>
 		</div>
 	</div>
-	
-	<div id="myModal" class="modal">
+
+
+													<!-- Login Modal -->
+
+		
+	<div id="Mymodal" class="modal">
 		<div class="modal-content">
-			<span class="close">&times;</span>
+			<span class="close1">&times;</span>
 			<div class="login">
 				<div class="login-screen">
 					<div class="app-title">
 						<h1>Login</h1>
 					</div>
 					<form method="post" action="login_process.php">
-					<div class="login-form">
-						<div class="control-group">
-						<input type="text" class="login-field" value="" placeholder="username" id="login-name" name="username">
-						<label class="login-field-icon fui-user" for="login-name"></label>
-						</div>
+						<div class="login-form">
+							<div class="control-group">
+									<input type="text" class="login-field" value="" placeholder="username" id="login-name" name="username">
+									<label class="login-field-icon fui-user" for="login-name"></label>
+							</div>
 
-						<div class="control-group">
-						<input type="password" class="login-field" value="" placeholder="password" id="login-pass" name="pswd">
-						<label class="login-field-icon fui-lock" for="login-pass"></label>
-						</div>
+							<div class="control-group">
+									<input type="password" class="login-field" value="" placeholder="password" id="login-pass" name="pswd">
+									<label class="login-field-icon fui-lock" for="login-pass"></label>
+							</div>
 
-						<input type="submit" class="btn btn-primary btn-large btn-block"  style="background-color:#1c2b4b" value="login"/>
-						<input type="button" class="btn btn-primary btn-large btn-block"  id="sign_up_btn" style="background-color:gray" value="Sign Up"/>
-						<a class="login-link" href="#"> Lost your password?</a>
-					</div>
+							<a class="login-link" href="#"> Lost your password?</a>
+						</div>
 					</form>
 				</div>
 			</div>
 		</div>
 	</div>
+
+
+													<!-- Sign up Modal -->
+
+
+	<div id="signup" class="modal">
+		<div class="modal-content">
+			<span class="close2">&times;</span>
+			<div class="login">
+				<div class="login-screen">
+					<div class="app-title">
+						<h1>Registration</h1>
+					</div>
+					<form method="post" action="#">
+						<div class="login-form">
+							<div class="control-group">
+									<input type="text" class="login-field" value="" placeholder="Name" name="Name">
+									<label class="login-field-icon fui-user" for="login-name"></label>
+							</div>
+							
+							<div class="control-group">
+									<input type="text" class="login-field" value="" placeholder="Registration no." name="regno">
+									<label class="login-field-icon fui-user" for="login-name"></label>
+							</div>
+
+
+							<div class="control-group">
+									<input type="text" class="login-field" value="" placeholder="Branch" id="login-name" name="branch">
+									<label class="login-field-icon fui-user" for="login-name"></label>
+							</div>
+
+
+							<div class="control-group">
+									<input type="text" class="login-field" value="" placeholder="Contact" id="login-name" name="contact">
+									<label class="login-field-icon fui-user" for="login-name"></label>
+							</div>
+
+
+							<div class="control-group">
+									<input type="text" class="login-field" value="" placeholder="Email" id="login-name" name="email">
+									<label class="login-field-icon fui-user" for="login-name"></label>
+							</div>
+
+
+
+							<div class="control-group">
+									<input type="text" class="login-field" value="" placeholder="Address" id="login-name" name="address">
+									<label class="login-field-icon fui-user" for="login-name"></label>
+							</div>
+
+
+							<input type="submit" class="btn btn-primary btn-large btn-block"  style="background-color:#1c2b4b" value="Submit"/>
+
+
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div> 
+
+
+
+
+
 	<script src="js/javascript.js"></script>
 </body>
-  
-  
-
-</body>
-
 </html>
--- js/javascript.js
+++ js/javascript.js
@@ -1,24 +1,42 @@
-var modal = document.getElementById('myModal');
+var modal = document.getElementById('Mymodal');
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var signup=document.getElementById('signup');
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var btn = document.getElementById("Btn");
 
-// When the user clicks on the button, open the modal 
+var signupbtn = document.getElementById("signupBtn");
+
+var span1 = document.getElementsByClassName("close1")[0];
+
+var span2 = document.getElementsByClassName("close2")[0];
+ 
 btn.onclick = function() {
     modal.style.display = "block";
 }
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
+span1.onclick = function() {
+    	modal.style.display="none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+span2.onclick = function() {
+    	signup.style.display="none";
+}
+
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+    else if(event.target==signup){
+    	signup.style.display="none";
+    }
 }
+
+			// For Sign up modal
+
+
+signupbtn.onclick = function() {
+    signup.style.display = "block";
+}
+
+
+
+
