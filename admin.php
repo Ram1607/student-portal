8ec83d0c2e00a564f80e42b79853c421bbe2ee49
pradumkr <pradumkumar321@live.com>
2019-04-03 23:41:16 +0530

    Admin edit is completed

--- admin.php
+++ admin.php
@@ -216,144 +216,191 @@
           <br>
           <br>
           <br>
+          <img style=" margin-left: 43%;
+         height: 175px;
+         width:175px;
+               " src="uploads/<?php echo $student['src_img'];?>"/>
+
 
             <table class="table" style="width: 60%">
-              <thead>
-                <tr>
-                  <th style="width: 80%; padding-left: 30%;" >  Personal Details   </th>
-                    <th style="width: 20%">     <button class ="btn-change" onclick="edit1()">Edit</button>  </th>
+            <thead>
+              <tr>
+                <th style="width: 80%; padding-left: 30%;" >  Personal Details   </th>
+                  <th style="width: 20%">     <button class ="btn-change" onclick="edit1()" id="sd_edit">Edit</button><button class ="btn-change" onclick="save()" id="ed_save" style="display: none;">Save</button></th>
 
 
-                </tr>
-              </thead>
-            </table >
+              </tr>
+            </thead>
+          </table >
 
 
-            <table class="table" style="width: 60%" id="edit1">
-              <tbody>
-                <tr>
-                    <th>Name</th>
-                    <td ><?php echo $student['name']; ?></td>
-                </tr>
-                <tr>
-                  <th>Father name</th>
-                  <td><?php echo $student['f_name']; ?></td> 
-                </tr>
-                <tr>
-                  <th >Mother Name</th>
-                  <td><?php echo $student['m_name']; ?></td>
-                </tr>
-                <tr>
-                  <th>Date of Birth</th>
-                  <td><?php echo $student['dob']; ?></td>
-                </tr>
-                
-                <tr>
-                  <th> Gender</th>
-                  <td><?php echo $student['gender']; ?></td>
-                </tr>  
-                    
-                <tr>
-                  <th>Nationality</th>
-                  <td><?php echo $student['nationality']; ?></td>
+          <table class="table" style="width: 60%" id="edit1">
+            <tbody>
+              <tr>
+                  <th>Name</th>
+                  <td ><input type="text" value="<?php echo $student['name']; ?>" style="text-align: center;display: none;" name="ed_name" id="ed_name"><span id="sd_name"><?php echo $student['name']; ?></span></td>
+              </tr>
+              <tr>
+                <th>Father name</th>
+                <td><input type="text" value="<?php echo $student['f_name']; ?>" style="text-align: center;display: none;" name="ed_fname" id="ed_fname"><span id="sd_fname"><?php echo $student['f_name']; ?></span></td> 
+              </tr>
+              <tr>
+                <th >Mother Name</th>
+                <td><input type="text" value="<?php echo $student['m_name']; ?>" style="text-align: center;display: none;" name="ed_mname" id="ed_mname"><span id="sd_mname"><?php echo $student['m_name']; ?></span></td>
+              </tr>
+              <tr>
+                <th>Date of Birth</th>
+                <td><input type="date" value="<?php echo $student['dob']; ?>" style="text-align: center;display: none;" name="ed_dob" id="ed_dob"><span id="sd_dob"><?php echo $student['dob']; ?></span></td>
+              </tr>
+              
+              <tr>
+                <th> Gender</th>
+                <td><div class=table id="ed_gender" style="display: none;">
+                      <table style="padding-left: 10px;">
+                        <tr>
+                          <td style="padding-left:38px;">
+                              <label for="male">Male</label>
+                              <input type="radio" name="gender" id="ed_male" value="0"<?php if($student['gender']==0){echo "checked";}?>>
+                          </td>
+                          <td style="padding-left: 38px;">
+                                <label for="female">Female</label>
+                              <input type="radio"  name="gender" id="ed_female" value="1" <?php if($student['gender']==1){echo "checked";}?>>
+                          </td>
+                        </tr>
+                        
+                      </table>
 
-                </tr>
+                      <span class="error_modal" style="display:none" id="error_gender">What's your gender ?</span>
 
+                  </div>
+                  <span id="sd_gender"><?php if($student['gender']==1){echo "female";}else{echo "male";}?></span></td>
+              </tr>  
+                  
+              <tr>
+                <th>Nationality</th>
+                <td><input type="text" value="<?php echo $student['nationality']; ?>" style="text-align: center;display: none;" name="ed_nationality" id="ed_nationality"><span id="sd_nationality"><?php echo $student['nationality']; ?></span></td>
 
-              </tbody>
-            </table>
-
-        <br>
-        <br>
-
-            <table class="table" style="width: 60%">
-              <thead>
-                <tr>
-
-                    <th style="width: 80%; padding-left: 30%;" >Education Details</th>
-                    <th style="width: 20%">     <button class ="btn-change" onclick="edit2()">Edit</button>  </th>
-
-                </tr>
-              </thead>
-            </table >
-
-
-            <table class="table" style="width: 60%" id=edit2>
-              <tbody>
-                <tr>
-                    <th>Registration no.  </th>
-                    <td ><?php echo $student['reg_no']; ?></td>
-                </tr>
-                <tr>
-                  <th>Roll no.</th>
-                  <td><?php echo $student['roll_no']; ?></td> 
-                </tr>
-                <tr>
-                  <th >Department</th>
-                  <td><?php echo $student['department']; ?></td>
-                </tr>
-                <tr>
-                  <th> Tenth Marks</th>
-                  <td><?php echo $student['marks_10']; ?></td>
-                </tr>
-                
-                <tr>
-                  <th> +2 Marks</th>
-                  <td><?php echo $student['marks_12']; ?></td>
-                </tr>  
-                    
-                <tr>
-                  <th>CGPA</th>
-                  <td><?php echo $student['cgpa']; ?></td>
-
-                </tr>
+              </tr>
 
 
-              </tbody>
-            </table>
+            </tbody>
+          </table>
 
-        <br>
-        <br>
+      <br>
+      <br>
 
-            <table class="table" style="width: 60%">
-              <thead>
-                <tr>
+          <table class="table" style="width: 60%">
+            <thead>
+              <tr>
 
-                    <th style="width: 80%; padding-left: 30%;" >Others</th>
-                    <th style="width: 20%">     <button class ="btn-change" onclick="edit3()">Edit</button>  </th>
+                  <th style="width: 80%; padding-left: 30%;" >Education Details</th>
+                  <th style="width: 20%">     <button class ="btn-change" onclick="edit2()" id="sd_edit1">Edit</button><button class ="btn-change" onclick="save()" id="ed_save1" style="display: none;">Save</button>  </th>
 
-                </tr>
-              </thead>
-            </table >
-
-
-            <table class="table" style="width: 60%" id="edit3">
-              <tbody>
-                <tr>
-                    <th>Contact</th>
-                    <td><?php echo $student['contact']; ?></td>
-                </tr>
-                <tr>
-                  <th>Email</th>
-                  <td><?php echo $student['email']; ?></td> 
-                </tr>
-                <tr>
-                  <th >Permanent Address</th>
-                  <td><?php echo $student['per_address']; ?></td>
-                </tr>
-                <tr>
-                  <th>Correspondent Address</th>
-                  <td><?php echo $student['cor_address']; ?></td>
-                </tr>
-                
+              </tr>
+            </thead>
+          </table >
 
 
-              </tbody>
-            </table>
+          <table class="table" style="width: 60%" id=edit2>
+            <tbody>
+              <tr>
+                  <th>Registration no.  </th>
+                  <td ><input type="text" style="display: none;text-align: center;" name="ed_reg_no" value="<?php echo $student['reg_no']; ?>" id="ed_reg_no"><span id="sd_reg_no" ><?php echo $student['reg_no']; ?></span></td>
+              </tr>
+              <tr>
+                <th>Roll no.</th>
+                <td><?php echo $student['roll_no']; ?></td> 
+              </tr>
+              <tr>
+                <th >Department</th>
+                <td><?php echo $student['department']; ?></td>
+              </tr>
+              <tr>
+                <th> Tenth Marks</th>
+                <td><input type="number" name="ed_marks_10" value="<?php echo $student['marks_10']; ?>" style="display: none;text-align: center;" id="ed_marks_10"><span id="sd_marks_10"><?php echo $student['marks_10']; ?></span></td>
+              </tr>
+              
+              <tr>
+                <th> +2 Marks</th>
+                <td><input type="number" name="ed_marks_12" value="<?php echo $student['marks_12']; ?>" style="display: none;text-align: center;" id="ed_marks_12"><span id="sd_marks_12"><?php echo $student['marks_12']; ?></span></td>
+              </tr>  
+                  
+              <tr>
+                <th>CGPA</th>
+                <td><input type="number" name="ed_cgpa" value="<?php echo $student['cgpa']; ?>" style="display: none;text-align: center;" id="ed_cgpa"><span id="sd_cgpa"><?php echo $student['cgpa']; ?></span></td>
+
+              </tr>
+
+
+            </tbody>
+          </table>
+
+      <br>
+      <br>
+
+          <table class="table" style="width: 60%">
+            <thead>
+              <tr>
+
+                  <th style="width: 80%; padding-left: 30%;" >Others</th>
+                  <th style="width: 20%">     <button class ="btn-change" onclick="edit3()" id="sd_edit2">Edit</button><button class ="btn-change" onclick="save()" id="ed_save2" style="display: none;">Save</button>  </th>
+
+              </tr>
+            </thead>
+          </table >
+
+
+          <table class="table" style="width: 60%" id="edit3">
+            <tbody>
+              <tr>
+                  <th>Contact</th>
+                  <td><input type="number" name="ed_contact" value="<?php echo $student['contact']; ?>" style="display: none;text-align: center;" id="ed_contact"><span id="sd_contact"><?php echo $student['contact']; ?></span></td>
+              </tr>
+              <tr>
+                <th>Email</th>
+                <td><input type="email" name="ed_email" value="<?php echo $student['email']; ?>" style="display: none;text-align: center;" id="ed_email"><span id="sd_email"><?php echo $student['email']; ?></span></td> 
+              </tr>
+              <tr>
+                <th >Permanent Address</th>
+                <td><input type="text" name="ed_per_address" value="<?php echo $student['per_address']; ?>" style="display: none;text-align: center;" id="ed_per_address"><span id="sd_per_address"><?php echo $student['per_address']; ?></span></td>
+              </tr>
+              <tr>
+                <th>Correspondent Address</th>
+                <td><input type="text" name="ed_cor_address" value="<?php echo $student['cor_address']; ?>" style="display: none; text-align: center;" id="ed_cor_address"><span id="sd_cor_address"><?php echo $student['cor_address']; ?></span></td>
+              </tr>
+              
+
+
+            </tbody>
+          </table>
+      <br>
+      <br>
+          <table class="table" style="width: 60%">
+            <thead>
+              <tr>
+
+                  <th style="width: 80%; padding-left: 30%;" >Additional Information</th>
+                  <th style="width: 20%">     <button class ="btn-change" onclick="edit4()" id="sd_edit3">Edit</button><button class ="btn-change" onclick="save1()" id="ed_save3" style="display: none;">Save</button>  </th>
+
+              </tr>
+            </thead>
+          </table >
+          <table class="table" style="width: 60%" id="edit3">
+            <tbody>
+              <tr>
+                <td id="add_info_row"><input type="text" name="add_info" style="width: 90%;height: 50%;" id="add_info" value="<?php echo $student['add_info'];?>"></td>
+              </tr>
+            </tbody>
+          </table>
 
         </div>
 
-     <?php } ?>
+     <?php  echo "<script>
+     window.onload = function(){
+     var add_info = document.getElementById('add_info').value;
+     document.getElementById('add_info_row').innerHTML = add_info;
+ }
+    </script>";}?>
   </div>
 
   <div id="txtHint">
@@ -361,7 +408,7 @@
 
 
     <script type="text/javascript" src="jquery.min.js"></script>
-  <script src="js/javascript.js"></script>
+  <script src="js/admin.js"></script>
 
 </body>
 </html>
---
+++ js/admin.js
@@ -0,0 +1,145 @@
+function ajaxreq(str,reg_no) {
+    var xhttp; 
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("txtHint").innerHTML = this.responseText;
+      location.reload();
+      }
+    };
+    xhttp.open("GET", "update.php?q="+str+"&reg="+reg_no, true);
+    xhttp.send();
+  }
+
+function edit1(){
+    document.getElementById('sd_name').style.display = "none";
+    document.getElementById('ed_name').style.display = "initial";
+    document.getElementById('sd_fname').style.display = "none";
+    document.getElementById('ed_fname').style.display = "initial";
+    document.getElementById('sd_mname').style.display = "none";
+    document.getElementById('ed_mname').style.display = "initial";
+    document.getElementById('sd_dob').style.display = "none";
+    document.getElementById('ed_dob').style.display = "initial";
+    document.getElementById('sd_gender').style.display = "none";
+    document.getElementById('ed_gender').style.display = "initial";
+    document.getElementById('sd_nationality').style.display = "none";
+    document.getElementById('ed_nationality').style.display = "initial";
+    document.getElementById('sd_edit').style.display = "none";
+    document.getElementById('ed_save').style.display = "initial";
+    document.getElementById('ed_save').style.backgroundColor = "red";
+}
+
+function edit2(){
+    document.getElementById('sd_marks_10').style.display = "none";
+    document.getElementById('ed_marks_10').style.display = "initial";
+    document.getElementById('sd_marks_12').style.display = "none";
+    document.getElementById('ed_marks_12').style.display = "initial";
+    document.getElementById('sd_cgpa').style.display = "none";
+    document.getElementById('ed_cgpa').style.display = "initial";
+    document.getElementById('sd_edit1').style.display = "none";
+    document.getElementById('ed_save1').style.display = "initial";
+    document.getElementById('ed_save1').style.backgroundColor = "red";
+}
+
+function edit3(){
+    document.getElementById('sd_contact').style.display = "none";
+    document.getElementById('ed_contact').style.display = "initial";
+    document.getElementById('sd_email').style.display = "none";
+    document.getElementById('ed_email').style.display = "initial";
+    document.getElementById('sd_per_address').style.display = "none";
+    document.getElementById('ed_per_address').style.display = "initial";
+    document.getElementById('sd_cor_address').style.display = "none";
+    document.getElementById('ed_cor_address').style.display = "initial";
+    document.getElementById('sd_edit2').style.display = "none";
+    document.getElementById('ed_save2').style.display = "initial";
+    document.getElementById('ed_save2').style.backgroundColor = "red";
+}
+
+function edit4(){
+    document.getElementById('sd_edit3').style.display = "none";
+    document.getElementById('ed_save3').style.display = "initial";
+    document.getElementById('ed_save3').style.backgroundColor = "red";
+    document.getElementById('ckeditor').style.display = "initial";
+    CKEDITOR.replace('ckeditor');
+    //CKEDITOR.instances['ckeditor'].setData(add_info);
+}
+
+function save() {
+    var name = document.getElementById('ed_name').value;
+    var fname = document.getElementById('ed_fname').value;
+    var mname = document.getElementById('ed_mname').value;
+    var reg_no = document.getElementById('ed_reg_no').value;
+    var dob = document.getElementById('ed_dob').value;
+    if(document.getElementById('ed_male').checked)
+    {
+        var gender = 0;
+    }
+    else
+    {
+        var gender = 1;
+    }
+    var nation = document.getElementById('ed_nationality').value;
+    var marks_10 = document.getElementById('ed_marks_10').value;
+    var marks_12 = document.getElementById('ed_marks_12').value;
+    var cgpa = document.getElementById('ed_cgpa').value;
+    var contact = document.getElementById('ed_contact').value;
+    var email = document.getElementById('ed_email').value;
+    var per_address = document.getElementById('ed_per_address').value;
+    var cor_address = document.getElementById('ed_cor_address').value;
+    var add_info = "";
+    console.log(add_info);
+
+    var xhttp; 
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+      alert(this.responseText);
+      location.reload();
+      }
+    };
+    console.log("name="+name+"&fname="+fname+"&mname="+mname+"&reg_no="+reg_no+"&dob="+dob+"&gender="+gender+"&nation="+nation+"&marks_10="+marks_10+
+        "&marks_12="+marks_12+"&cgpa="+cgpa+"&contact="+contact+"&email="+email+"&per_address="+per_address+"&cor_address="+cor_address+"&add_info="+add_info);
+    xhttp.open("GET", "edit.php?name="+name+"&fname="+fname+"&mname="+mname+"&reg_no="+reg_no+"&dob="+dob+"&gender="+gender+"&nation="+nation+"&marks_10="+marks_10+
+        "&marks_12="+marks_12+"&cgpa="+cgpa+"&contact="+contact+"&email="+email+"&per_address="+per_address+"&cor_address="+cor_address+"&add_info="+add_info, true);
+    xhttp.send();
+}
+
+function save1() {
+    var name = document.getElementById('ed_name').value;
+    var fname = document.getElementById('ed_fname').value;
+    var mname = document.getElementById('ed_mname').value;
+    var reg_no = document.getElementById('ed_reg_no').value;
+    var dob = document.getElementById('ed_dob').value;
+    if(document.getElementById('ed_male').checked)
+    {
+        var gender = 0;
+    }
+    else
+    {
+        var gender = 1;
+    }
+    var nation = document.getElementById('ed_nationality').value;
+    var marks_10 = document.getElementById('ed_marks_10').value;
+    var marks_12 = document.getElementById('ed_marks_12').value;
+    var cgpa = document.getElementById('ed_cgpa').value;
+    var contact = document.getElementById('ed_contact').value;
+    var email = document.getElementById('ed_email').value;
+    var per_address = document.getElementById('ed_per_address').value;
+    var cor_address = document.getElementById('ed_cor_address').value;
+    var add_info = encodeURI(CKEDITOR.instances['ckeditor'].getData());
+    console.log(add_info);
+
+    var xhttp; 
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+      alert(this.responseText);
+      location.reload();
+      }
+    };
+    console.log("name="+name+"&fname="+fname+"&mname="+mname+"&reg_no="+reg_no+"&dob="+dob+"&gender="+gender+"&nation="+nation+"&marks_10="+marks_10+
+        "&marks_12="+marks_12+"&cgpa="+cgpa+"&contact="+contact+"&email="+email+"&per_address="+per_address+"&cor_address="+cor_address+"&add_info="+add_info);
+    xhttp.open("GET", "edit.php?name="+name+"&fname="+fname+"&mname="+mname+"&reg_no="+reg_no+"&dob="+dob+"&gender="+gender+"&nation="+nation+"&marks_10="+marks_10+
+        "&marks_12="+marks_12+"&cgpa="+cgpa+"&contact="+contact+"&email="+email+"&per_address="+per_address+"&cor_address="+cor_address+"&add_info="+add_info, true);
+    xhttp.send();
+}
